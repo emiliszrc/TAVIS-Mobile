6de90fc6cdea6c5447a9c88d3cc21ffa2ed68408
emzar <em.zareckis@gmail.com>
2021-05-06 23:25:56 +0300

    main layout push

--- TravelManagerPrototype/TravelManagerPrototype/Main.xaml.cs
+++ TravelManagerPrototype/TravelManagerPrototype/Main.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Humanizer;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XF.Material.Forms.UI.Dialogs;
@@ -47,6 +47,146 @@ namespace TravelManagerPrototype
             }
 
             CheckinList.ItemsSource = checkinRequestItems;
+
+            UpdateMainPageDetails();
+        }
+
+        private void UpdateMainPageDetails()
+        {
+            var currentTime = DateTime.UtcNow;
+            Visit previousVisit = null;
+            Visit nextVisit = null;
+            Visit nextAccomodation = null;
+            TimeSpan alreadyTravelingFor = TimeSpan.Zero;
+            TimeSpan leftUntilNextStop = TimeSpan.Zero;
+
+            foreach (var tripVisit in trip.Visits)
+            {
+                TimeSpan timeBetween = TimeSpan.Zero;
+                TimeSpan timeTraveling = TimeSpan.Zero;
+
+                if (trip.Visits.IndexOf(tripVisit) > 0)
+                {
+                    var previous = trip.Visits[trip.Visits.IndexOf(tripVisit) - 1];
+                    var next = trip.Visits[trip.Visits.IndexOf(tripVisit)];
+
+                    timeTraveling = DateTime.UtcNow - previous.Departure;
+                    timeBetween = next.Arrival - previous.Departure;
+                }
+
+                var stringed = tripVisit.Departure.ToString();
+                var stringedA = tripVisit.Arrival.ToString();
+                var current = currentTime.ToString();
+                if (tripVisit.Arrival >= currentTime && trip.Visits[trip.Visits.IndexOf(tripVisit) - 1].Departure <= currentTime)
+                {
+                    nextVisit = tripVisit;
+                    previousVisit = trip.Visits[trip.Visits.IndexOf(nextVisit) - 1];
+                    alreadyTravelingFor = timeTraveling;
+
+                    if (timeBetween < TimeSpan.FromHours(3))
+                    {
+                        leftUntilNextStop = timeBetween - timeTraveling;
+                    }
+                    else
+                    {
+                        var stopsAt = new List<DateTime>();
+
+                        var timeBtwn = timeBetween;
+
+                        while (true)
+                        {
+
+                            if (timeBtwn > TimeSpan.FromHours(3))
+                            {
+                                timeBtwn = timeBtwn - TimeSpan.FromHours(3);
+                                if (stopsAt.Any())
+                                {
+                                    stopsAt.Add(stopsAt.LastOrDefault() + TimeSpan.FromHours(3));
+                                }
+                                else
+                                {
+                                    stopsAt.Add(previousVisit.Departure + TimeSpan.FromHours(3));
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        foreach (var dateTime in stopsAt)
+                        {
+                            if (stopsAt.IndexOf(dateTime) > 0)
+                            {
+                                var prevStop = stopsAt[stopsAt.IndexOf(dateTime) - 1];
+
+                                if (currentTime > prevStop && currentTime < dateTime)
+                                {
+                                    leftUntilNextStop = dateTime - currentTime;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    break;
+                }
+            }
+
+            foreach (var tripVisit in trip.Visits)
+            {
+                if (tripVisit.Arrival > currentTime && tripVisit.Location.Type == "lodging")
+                {
+                    nextAccomodation = tripVisit;
+                    break;
+                }
+            }
+
+            if (nextVisit != null)
+            {
+                upcomingVisitAddress.Text = nextVisit.Location.Address;
+                upcomingVisitTitle.Text = nextVisit.Location.Title;
+                upcomingVisitTimeLeft.Text = (nextVisit.Arrival - currentTime).Humanize();
+            }
+            else
+            {
+                upcomingVisitLabel.Text = "No visits left";
+                upcomingVisitAddress.Text = "-";
+                upcomingVisitTitle.Text = "-";
+                upcomingVisitTimeLeft.Text = "-";
+            }
+
+            if (nextAccomodation != null)
+            {
+                upcomingHotelAddress.Text = nextAccomodation.Location.Address;
+                upcomingHotelTitle.Text = nextAccomodation.Location.Title;
+                upcomingHotelTimeLeft.Text = (nextAccomodation.Arrival - currentTime).Humanize();
+            }
+            else
+            {
+                upcomingHotelLabel.Text = "No hotels left";
+                upcomingHotelAddress.Text = "-";
+                upcomingHotelTitle.Text = "-";
+                upcomingHotelTimeLeft.Text = "-";
+            }
+
+            if (leftUntilNextStop != TimeSpan.Zero)
+            {
+                upcomingStopTiming.Text = leftUntilNextStop.Humanize();
+            }
+            else
+            {
+                upcomingStopTiming.Text = "-";
+            }
+
+            if (previousVisit != null)
+            {
+                feedbackLabel.Text = $"You just visited {previousVisit.Location.Title}, let us know what you think!";
+            }
+            else
+            {
+                feedbackLabel.Text = "";
+            }
         }
 
         private async void SendNavigation(object sender, ItemTappedEventArgs e)
